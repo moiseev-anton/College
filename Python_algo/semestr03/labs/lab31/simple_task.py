ecfa6b2b9c628b310fd37dd428a07418c306a6e0
anton moiseev <moucee13@gmail.com>
2024-09-02 13:51:38 +0300

    correct comment in algo lab31

--- Python_algo/semestr03/labs/lab31/simple_task.py
+++ Python_algo/semestr03/labs/lab31/simple_task.py
@@ -3,24 +3,26 @@ class Cube:
         """Инициализация экземпляра куба"""
         if side_length < 0:
             raise ValueError('Сторона не может быть отрицательной')
-        self.side_length = side_length
+
+        self.side_length = side_length # устанавливаем значение атрибута куба
 
     def get_face_area(self):
-        """Вычисление площади грани"""
+        """Метод вычисления площади грани"""
         return self.side_length ** 2
 
     def get_surfase_area(self):
-        """Вычисление площади поверхности"""
+        """Метод вычисления площади поверхности"""
         return self.get_face_area() * 6
 
     def get_volume(self):
-        """Вычисление объема"""
+        """Метод вычисления объема"""
         return self.side_length ** 3
 
 
 if __name__ == '__main__':
     try:
         side_length = float(input("Введите длину стороны куба: "))
+        # создаем экземпляр с полученным значением
         cube = Cube(side_length)
         print(f"Площадь грани: {cube.get_face_area()}")
         print(f"Площадь поверхности: {cube.get_surfase_area()}")
